e7f2a0d62245f2dd520fb2459283d287cc803312
A.Glazyrin <alex_glazyrin@yahoo.com>
2023-04-12 13:59:49 +0300

    hw1 в процессе

---
+++ hw1.js
@@ -0,0 +1,35 @@
+// 1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
+
+const SuperLinkEl = document.getElementById('super_link');
+console.log(SuperLinkEl);
+
+// 2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
+
+const CardLinkEl = document.querySelectorAll('.card-link');
+
+CardLinkEl.forEach(element => {
+    element.textContent = 'ссылка';
+    console.log(element);
+});
+
+
+// 3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
+
+const LinkByBodyEl = document.querySelectorAll('.card-body .card-link');
+console.log(LinkByBodyEl);
+
+// 4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
+//Спорное решение (подумать).
+
+const dataNumber50El = document.querySelector('[data-number="50"]');
+console.log(dataNumber50El);
+
+// 5. Выведите содержимое тега title в консоль.
+
+const entTitle = document.querySelector('title');
+console.log(entTitle.textContent);
+
+// 6. Получите элемент с классом "card-title" и выведите его родительский узел в консоль.
+
+const cardTitleAnces = document.querySelector('.card-title');
+console.log(cardTitleAnces.parentNode); 
